e7238346119b06dae8e59369398b9e31281e2067
Zi <hungryzi@gmail.com>
2017-03-05 13:44:40 +0700

    Added testing

---
+++ backend/spec/api/api_spec.rb
@@ -0,0 +1,9 @@
+describe API do
+  context 'GET /api/greeting' do
+    it 'returns a greeting' do
+      get '/api/greeting'
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)).to eq('greeting' => 'Hello!')
+    end
+  end
+end
